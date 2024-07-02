dd76632df2d126f513d0a288997884c8ce549e67
karakasal <kalkontakt@outlook.com>
2024-07-02 21:30:35 +0300

    Add files via upload

---
+++ img_converter.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import sys
+import os
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f"{image_folder}{filename}")
+    clean_img = os.path.splitext(filename)[0]
+    img.save(f"{output_folder}{clean_img}.png")
+    
+print("Done")
